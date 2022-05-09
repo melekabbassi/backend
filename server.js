296323f5854403df394fe18bf8f43a86056c8695
melekabbassi <abbassimelek@gmail.com>
2022-05-09 14:48:44 +0100

    added post method

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// importations
+const { response } = require('express');
 const express = require('express');
 const app = express();
 
@@ -5,6 +7,9 @@ const router = express.Router();
 
 const mongoose = require('mongoose');
 
+const Thing = require('./models/thing');
+
+// Connect to the database
 mongoose.connect('mongodb+srv://<Octopus>:<KvIRFsLQ3lemWdrP>@cluster0.hhvvy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', (error, done) => {
     if (error) {
         console.log(error);
@@ -13,17 +18,38 @@ mongoose.connect('mongodb+srv://<Octopus>:<KvIRFsLQ3lemWdrP>@cluster0.hhvvy.mong
     }
 });
 
+// Post Method
+app.post('/add_thing', async (request, response) => {
+    try {
+        let newThing = new Thing({
+            title: request.body.title,
+            description: request.body.description,
+            imageUrl: request.body.imageUrl,
+            userId: request.body.userId,
+            price: request.body.price
+        });
+        await newThing.save();
+        response.send("Saved Successfully");
+    } catch (error) {
+        response.send(error);
+    }
+});
+
+// Get Method
 router.get('/irm', (request, response) => {
     response.send('IRM');
 });
 
+// Middleware
 app.use('/', router)
 
+// Middleware
 app.use((request, response, next) => {
     response.status(404).send('404 Not Found');
     next();
 });
 
+// listen on port 3000 and log the server is running
 app.listen(3000, (request, response) => {
     console.log('Server is running on port 3000');
     }
