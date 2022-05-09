e432a5b9031a0e3f0ffacb2071c8e344ebc03e80
melekabbassi <abbassimelek@gmail.com>
2022-05-09 13:42:14 +0100

    gets itm

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 
+const router = express.Router();
+
+router.get('/irm', (req, res) => {
+    res.send('IRM');
+});
+
+app.use('/', router)
+
 app.listen(3000, (request, response) => {
     console.log('Server is running on port 3000');
     }
