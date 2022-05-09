fdc7f7c65922889be8819ad58abef4b2dfd38cd3
melekabbassi <abbassimelek@gmail.com>
2022-05-09 14:21:17 +0100

    linked database

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const app = express();
 
 const router = express.Router();
 
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://<Octopus>:<KvIRFsLQ3lemWdrP>@cluster0.hhvvy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', (error, done) => {
+    if (error) {
+        console.log(error);
+    } else if (done) {
+        console.log('MongoDB Connected...');
+    }
+});
+
 router.get('/irm', (request, response) => {
     response.send('IRM');
 });
